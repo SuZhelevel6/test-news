2ccce48ebe4f559a9884ed9c7a30f6a678379290
suzhe <2212294193@qq.com>
2025-02-04 18:04:23 +0800

    创建新项目
    1. 本项目是传统安卓项目
    2. 关闭compose功能
    3. 创建基本的SplashActivity，并作为启动Activity。

---
+++ app/src/main/java/com/suzhe/news/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.suzhe.news.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.suzhe.news.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
