989fe652bd6e2ca9dd7591bba6a6227201e91aaa
suzhe <2212294193@qq.com>
2025-02-04 20:05:52 +0800

    启动界面-沉浸式状态栏
    1. 实现沉浸式状态栏，设置状态栏文字黑色

--- app/src/main/java/com/suzhe/news/splash/SplashActivity.kt
+++ app/src/main/java/com/suzhe/news/splash/SplashActivity.kt
@@ -2,11 +2,18 @@ package com.suzhe.news.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper
 import com.suzhe.news.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //设置沉浸式状态栏
+        QMUIStatusBarHelper.translucent(this)
+
+        //状态栏文字黑色
+        QMUIStatusBarHelper.setStatusBarLightMode(this)
     }
 }
