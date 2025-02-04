ae881a6fdfc314209b52118637cb45a6c4ce9f60
suzhe <2212294193@qq.com>
2025-02-04 20:05:52 +0800

    启动界面-界面布局
    1. 实现沉浸式状态栏，设置状态栏文字黑色

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //UI框架，主要是用他的工具类，也可以单独拷贝出来
+    //https://qmuiteam.com/android/get-started
+    implementation ("com.qmuiteam:qmui:2.1.0")
 }
