063366d32869c9297535feb8ea606490805f5088
suzhe <2212294193@qq.com>
2025-02-05 14:42:32 +0800

    启动界面-深色模式和浅色模式
    1. 添加识别深色浅色模式的工具类

--- app/src/main/java/com/suzhe/news/splash/SplashActivity.kt
+++ app/src/main/java/com/suzhe/news/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper
 import com.suzhe.news.R
+import com.suzhe.superui.util.SuperDarkUtil
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,12 @@ class SplashActivity : AppCompatActivity() {
         //设置沉浸式状态栏
         QMUIStatusBarHelper.translucent(this)
 
-        //状态栏文字黑色
-        QMUIStatusBarHelper.setStatusBarLightMode(this)
+        if (SuperDarkUtil.isDark(this)) {
+            //状态栏文字白色
+            QMUIStatusBarHelper.setStatusBarDarkMode(this)
+        } else {
+            //状态栏文字黑色
+            QMUIStatusBarHelper.setStatusBarLightMode(this)
+        }
     }
 }
