5fee207db21ed7612bf8a119143e4dc435d0f717
suzhe <2212294193@qq.com>
2025-02-05 22:23:38 +0800

    封装通用控制器-获取动态权限
    1. 在获取相册的时候如果没选好像会闪退

--- app/src/main/java/com/suzhe/news/splash/SplashActivity.kt
+++ app/src/main/java/com/suzhe/news/splash/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.suzhe.news.splash
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.Manifest
+import com.permissionx.guolindev.PermissionX
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper
 import com.suzhe.news.R
 import com.suzhe.news.activity.BaseLogicActivity
@@ -34,15 +36,47 @@ class SplashActivity : BaseLogicActivity() {
         super.initDatum()
         if (DefaultPreferenceUtil.getInstance(this).isAcceptTermsServiceAgreement) {
             //已经同意了用户协议
-
+            requestPermission()
         } else {
             showTermsServiceAgreementDialog()
         }
     }
 
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            PermissionX.init(this).permissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_MEDIA_AUDIO,
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.READ_MEDIA_VIDEO,
+            )
+        } else {
+            PermissionX.init(this).permissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            )
+        }.request { allGranted, grantedList, deniedList ->
+            if (allGranted) {
+                Log.d("SplashActivity", "All permissions are granted!")
+            } else {
+                //可以在这里弹出提示告诉用户为什么需要权限
+                finish()
+            }
+        }
+    }
     private fun showTermsServiceAgreementDialog() {
         TermServiceDialogFragment.show(supportFragmentManager) {
             Log.d("TAG", "primary OnClick")
+            requestPermission()
         }
     }
 }
