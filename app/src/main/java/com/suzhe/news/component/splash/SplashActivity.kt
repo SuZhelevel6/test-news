3006827547ea4dd91525cc7840ba3acc792cbb9a
suzhe <2212294193@qq.com>
2025-02-06 20:26:50 +0800

    重构-创建component包

--- app/src/main/java/com/suzhe/news/component/splash/SplashActivity.kt
+++ app/src/main/java/com/suzhe/news/component/splash/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.suzhe.news.splash
+package com.suzhe.news.component.splash
 
 import android.os.Build
 import android.util.Log
--- app/src/main/java/com/suzhe/news/component/splash/TermServiceDialogFragment.kt
+++ app/src/main/java/com/suzhe/news/component/splash/TermServiceDialogFragment.kt
@@ -1,4 +1,4 @@
-package com.suzhe.news.splash
+package com.suzhe.news.component.splash
 
 import android.os.Bundle
 import android.text.Html
